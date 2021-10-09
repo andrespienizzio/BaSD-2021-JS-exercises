31ea2dfdde95f916c75807820ffcda0c5e8ff95c
andrespienizzio <andrespienizzio@gmail.com>
2021-10-09 14:47:18 -0300

    add html h1 & excercise 1 js

---
+++ excercises/variables-and-operators-01.js
@@ -0,0 +1,29 @@
+/*****************************
+* 1) Variables and operators
+******************************/
+
+/* a) Create two numeric variables and use the sum operator to store the value of the sum of both 
+numbers in a third variable */
+
+var num1 = 2;
+var num2 = 3;
+var sum = num1 + num2;
+
+console.log(sum);
+
+/* b) Create two String variables and concatenate them by storing the result in a third variable */
+
+var firstname = 'Andres ';
+var lastname = 'Pienizzio';
+var name = firstname + lastname;
+
+console.log(name);
+
+/* c) Create two String variables and sum the length of each variable (number of letters of the string), 
+saving the result of the sum in a third variable (use length) */
+
+var string1 = 'Andres ';
+var string2 = 'Pienizzio';
+var stringslength = string1.length + string2.length;
+
+console.log(stringslength);
