535264cbd456549260264c913366f448f1a3a8fc
andrespienizzio <andrespienizzio@gmail.com>
2021-10-11 06:01:44 -0300

    Remove duplicated JS files - add 05 and 06 JS blank files

---
+++ excercises/05-for.js
@@ -0,0 +1,25 @@
+/*****************************
+* 5) For
+******************************/
+
+/* a) Create two numeric variables and use the sum operator to store the value of the sum of both 
+numbers in a third variable */
+
+var num1 = 2;
+var num2 = 3;
+var sum = num1 + num2;
+
+console.log(sum);
+
+/* a) Create an array that contains 5 words and loop through that array using JavaScript For to show an alert
+using each of the words. */
+
+/* b) To the previous array, convert the first letter of each word to uppercase and show an alert for each 
+modified word. */
+
+/* c) Create a variable called "sentence" that has an empty string, then go through the array from point a) with 
+a For loop to save each word within the sentence variable. At the end show a single alert with the complete chain. */
+
+/* d) Create an empty array with a For loop of 10 repetitions. Fill the array with the repetition number, that is,
+at the end of the execution of the for loop there should be 10 elements within the array, from number 0 to number 9. 
+Show the final array through the browser console (use console .log). */
---
+++ excercises/06-functions.js
@@ -0,0 +1,19 @@
+/*****************************
+* 6) Functions
+******************************/
+
+/* a) Create a sum function that receives two numerical values ​​and returns the result. Execute the function 
+and save the result in a variable, showing the value of the variable in the browser console. */
+
+
+/* b) To the previous sum function, add a validation to control if any of the parameters is not a number, 
+show an alert clarifying that one of the parameters has an error and return the NaN value as a result. */
+
+/* c) Create a validate integer function that receives a number as a parameter and returns true if it is 
+an integer number. */
+
+/* d) To the sum function of exercise 6b) add a call that validates that the numbers are integers. In case 
+there are decimals, show an alert with the error and return the number converted to integer number (rounded). */
+
+/* e) Convert the validation of exercise 6b) into a separate function and call it within the sum function 
+testing that everything continues to work the same. */
