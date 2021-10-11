835e5f3c50fc7121309ca727d5a38ac9b4361170
andrespienizzio <andrespienizzio@gmail.com>
2021-10-11 03:47:10 -0300

    Modify var names excercise 02

--- excercises/strings-02.js
+++ excercises/strings-02.js
@@ -4,66 +4,66 @@
 
 /* a) Create a string variable with at least 10 characters and convert all the text to uppercase (use UpperCase). */
 
-var string2A = 'República Argentina';
+var string_2A = 'República Argentina';
 
-console.log(string2A.toUpperCase());
+console.log(string_2A.toUpperCase());
 
 /* b) Create a string type variable with at least 10 characters and generate a new string with the first 5 characters 
 storing the result in a new variable (use substring). */
 
-var string2B = 'Confederación Argentina';
+var string_2B = 'Confederación Argentina';
 
-var str5Char = string2B.substring(0,5);
+var str_5_Char = string_2B.substring(0,5);
 
-console.log(str5Char);
+console.log(str_5_Char);
 
 /* c) Create a string type variable with at least 10 characters and generate a new string with the last 3 characters 
 storing the result in a new variable (use substring). */
 
-var string2C = 'Provincias Unidas del Río de la Plata';
+var string_2C = 'Provincias Unidas del Río de la Plata';
 
-var strLast3Char = string2C.substring(34);
+var str_Last_3Char = string_2C.substring(34);
 
 // var strLast3Char = string2c.substring(string2c.length-3); -alternative-
 
-console.log(strLast3Char);
+console.log(str_Last_3Char);
 
 /* d) Create a string type variable with at least 10 characters and generate a new string with uppercase in the first 
 letter and lowercase in the rest. Save the result in a new variable (use substring, toUpperCase, toLowerCase, and 
 the + operator). */
 
-var string2D = 'nación Argentina';
+var string_2D = 'nación Argentina';
 
-var string2DMay = string2D.substring(0,1).toUpperCase();
+var str_2D_Upp = string_2D.substring(0,1).toUpperCase();
 
-var string2DMin = string2D.substring(1).toLowerCase();
+var str_2D_Low = string_2D.substring(1).toLowerCase();
 
 // var string2DFinal2 = string2D.substring(0,1).toUpperCase() + string2D.substring(1).toLowerCase(); -simplification-
 
-var string2DFinal = string2DMay + string2DMin;
+var str_2D_Final = str_2D_Upp + str_2D_Low;
 
-console.log(string2DFinal);
+console.log(str_2D_Final);
 
 /* e) Create a string type variable with at least 10 characters and one blank space. Find the position of the first 
 blank space and save it in a new variable (use indexOf). */
 
-var string2E = 'Virreinato del Rio de la Plata';
+var string_2E = 'Virreinato del Rio de la Plata';
 
-var string2EBlank = string2E.indexOf(' ');
+var str_2E_Blank = string_2E.indexOf(' ');
 
-console.log(string2EBlank);
+console.log(str_2E_Blank);
 
 /* f) Create a string type variable with at least 2 long words (10 characters and some space in between). Use the 
 methods from the previous exercises to generate a new string containing the main letter of both words in uppercase 
 and the rest in lowercase letters (use indexOf, substring, toUpperCase, toLowerCase, and the + operator). */
 
-var string2F = 'CONSTITUCION NACIONAL';
+var string_2F = 'CONSTITUCIÓN NACIONAL';
 
-console.log(string2F.indexOf(' '));
+console.log(string_2F.indexOf(' '));
 
-var string2FMin = string2F.toLowerCase();
+var str_2F_Low = string_2F.toLowerCase();
 
-var string2FSolution = string2FMin.substring(0,1).toUpperCase() + string2FMin.substring(1,13) +
-                       string2FMin.substring(13,14).toUpperCase() + string2FMin.substring(14);
+var str_2F_Final = str_2F_Low.substring(0,1).toUpperCase() + str_2F_Low.substring(1,13) +
+                   str_2F_Low.substring(13,14).toUpperCase() + str_2F_Low.substring(14);
 
-console.log(string2FSolution);
+console.log(str_2F_Final);
